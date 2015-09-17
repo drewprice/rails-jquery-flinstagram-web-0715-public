95064f71c3b5b35bed3bceaa30cb631bb6f43f07
Drew Price <drewprice@users.noreply.github.com>
2015-09-17 13:27:45 -0400

    Remove respond_to block

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,11 +7,5 @@ class PicturesController < ApplicationController
   def like
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-
-    respond_to do |format|
-      format.js
-      format.html { redirect_to root_path  }
-      format.json { render json: @resource }
-    end
   end
 end
